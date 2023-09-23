dd29e2677454ad3ff6fad317c079c65b10602182
Renata Reinartz <rcreinartz@gmail.com>
2023-09-23 14:08:48 -0600

    changed port for process.env

--- index.js
+++ index.js
@@ -74,6 +74,8 @@ app.use((error, req, res, next) => {
   res.send({ success: false, error: error.message });
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
